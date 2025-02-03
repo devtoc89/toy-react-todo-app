aa9731eded4fee0c46e57cdd594d2bf0f554bd41
devtoc89 <devtoc89@gmail.com>
2025-02-03 13:16:22 +0900

    fix: 토스트 지속 시간 감소소

--- src/utils/alert.util.ts
+++ src/utils/alert.util.ts
@@ -5,21 +5,22 @@ import { toast } from 'react-toastify'
  * toast 처리를 위한 유틸
  */
 // toast debounce handler
+const toastifyCommonOption = { autoClose: 1000 }
 const toastDebounce = debounce((callback) => callback(), 200)
 
 // toat 유틸
 const ttoast = {
   success: (msg: string) => {
-    toastDebounce(() => toast.success(msg))
+    toastDebounce(() => toast.success(msg, { ...toastifyCommonOption }))
   },
   warning: (msg: string) => {
-    toastDebounce(() => toast.warning(msg))
+    toastDebounce(() => toast.warning(msg, { ...toastifyCommonOption }))
   },
   error: (msg: string) => {
-    toastDebounce(() => toast.error(msg))
+    toastDebounce(() => toast.error(msg, { ...toastifyCommonOption }))
   },
   info: (msg: string) => {
-    toastDebounce(() => toast.info(msg))
+    toastDebounce(() => toast.info(msg, { ...toastifyCommonOption }))
   },
 }
 
